66d74a31495415f77ebbb011e960a7ca284962bd
Fernando Borretti <eudoxiahp@gmail.com>
2014-05-06 19:57:05 -0300

    Add ASDF code

--- src/asdf.lisp
+++ src/asdf.lisp
@@ -1,1 +1,16 @@
 (in-package :clos-fixtures)
+
+(defclass fixture (source-file)
+  ((type :initform "lisp")))
+
+(defmethod perform ((o load-op) (component fixture)) t)
+
+(defmethod output-files ((operation compile-op) (component fixture))
+  nil)
+
+(defmethod perform ((o compile-op) (component fixture))
+  (let ((pathname (component-pathname component)))
+    (format t "Loading fixture '~A~&" (pathname-name pathname))
+    (load-fixtures (slurp-file pathname))))
+
+(import 'fixture :asdf)
