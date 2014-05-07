6826038b50f45ade6ed8dbb140f38e7f9e9bb047
Fernando Borretti <eudoxiahp@gmail.com>
2014-05-07 19:14:49 -0300

    Change methods

--- t/clos-fixtures.lisp
+++ t/clos-fixtures.lisp
@@ -1,18 +1,18 @@
 (in-package :cl-user)
 (defpackage clos-fixtures-test
-  (:use :cl :clos-fixtures))
+  (:use :cl))
 (in-package :clos-fixtures-test)
 
 (defclass city ()
   ((name  :type string :initarg :name :reader name)
    (state :type string :initarg :state :reader state)))
 
-(defmethod add-city ((city city))
+(defmethod clos-fixtures:register-fixture ((city city))
   (format t "Adding ~A, ~A.~&" (name city) (state city)))
 
 (defclass molecule ()
   ((name :type string :initarg :name :reader name)
    (formula :type string :initarg :formula :reader formula)))
 
-(defmethod add-molecule ((molecule molecule))
-  (format t "Adding ~A, ~A.~&" (name molecule) (formula molecule)))
+(defmethod clos-fixtures:register-fixture ((molecule molecule))
+  (format t "Adding ~A (~A).~&" (name molecule) (formula molecule)))
