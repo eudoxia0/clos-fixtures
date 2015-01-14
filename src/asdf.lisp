e32b61a1504069548653bb463c68d4ebdd7f2d33
Fernando Borretti <eudoxiahp@gmail.com>
2015-01-14 16:32:25 -0200

    Fix #1, simplify some stuff

--- src/asdf.lisp
+++ src/asdf.lisp
@@ -2,9 +2,10 @@
 
 (defclass fixture (source-file)
   ((type :initform "lisp")
-   (package :initform nil
-            :initarg :package
-            :reader fixture-package)))
+   (package-name :reader fixture-package-name
+                 :initarg :package
+                 :initform :cl
+                 :documentation "The name of the package to read the forms in.")))
 
 (defmethod perform ((o load-op) (component fixture)) t)
 
@@ -14,7 +15,6 @@
 (defmethod perform ((o compile-op) (component fixture))
   (let ((pathname (component-pathname component)))
     (format t "~&Loading fixture '~A'" (pathname-name pathname))
-    (load-fixtures (read-fixtures pathname)
-                   (fixture-package component))))
+    (load-fixtures pathname (fixture-package-name component))))
 
 (import 'fixture :asdf)
--- src/clos-fixtures.lisp
+++ src/clos-fixtures.lisp
@@ -4,27 +4,19 @@
   (:export :register-fixture))
 (in-package :clos-fixtures)
 
-(defun read-fixtures (path)
-  ;; Credit: http://www.ymeme.com/slurping-a-file-common-lisp-83.html
-  (with-open-file (stream path)
-    (let ((seq (make-array (+ 2 (file-length stream))
-                           :element-type 'character
-                           :fill-pointer t
-                           :adjustable t)))
-      (setf (elt seq 0) #\()
-      (setf (fill-pointer seq) (read-sequence seq stream :start 1))
-      (setf (elt seq (1- (fill-pointer seq))) #\))
-      (read-from-string seq))))
+(defun read-fixtures (path package)
+  (uiop:with-safe-io-syntax (:package package)
+    (uiop:read-file-forms path)))
 
 (defgeneric register-fixture (instance)
   (:documentation "Register a fixture."))
 
-(defun load-fixtures (fixtures &optional package)
-  (loop for fixture in fixtures do
-    (let ((class (if package
-                     (intern (symbol-name (first fixture)) package)
-                     (first fixture))))
-      (loop for fixture-params in (rest fixture) do
-        (register-fixture (apply #'make-instance
-                                 (append (list class)
-                                         fixture-params)))))))
+(defun load-fixtures (pathname package-name)
+  (let* ((package (find-package package-name))
+         (fixtures (read-fixtures pathname package)))
+    (loop for fixture in fixtures do
+      (let ((class (first fixture)))
+        (loop for fixture-params in (rest fixture) do
+          (register-fixture (apply #'make-instance
+                                   (append (list class)
+                                           fixture-params))))))))
